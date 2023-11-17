0d50c72dc428e376e8dd1d870387b30f8c6088b8
Shadrak Lagadi <shadrl19@gmail.com>
2023-11-17 12:04:43 -0700

    <assertEqual complete>

---
+++ assertEquals.js
@@ -0,0 +1,16 @@
+// 1) If the values match, print "Assertion Passed: [actual] === [expected]" values in []
+// 2)Otherwise it should, print "Assertion Failed: [actual] !== [expected]" values in []
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😊Assertion Passed: " + [actual] + " === " + [expected]);
+  } else {
+    console.log("😢Assertion Failed: " + [actual] + " !== " + [expected]);
+  }
+};
+
+// TEST CODE
+assertEqual("Shad", 123);
+assertEqual(123, 123);
