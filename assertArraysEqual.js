51057ed4d8903c7afa26456e26f93a6979e21b5e
Shadrak Lagadi <shadrl19@gmail.com>
2023-11-18 16:36:11 -0700

    lotide w01 commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArrayEqual = function(given, needed) {
+  const arraysEqual = eqArrays(given, needed);
+  if (arraysEqual) {
+    console.log(`😊Assertion Passed: [${given}] === [${needed}]`);
+  } else {
+    console.log(`😢Assertion Failed: ${given} !== ${needed}`);
+  }
+};
+
+assertArrayEqual([1, 2], [1, 2, 3]);
+
