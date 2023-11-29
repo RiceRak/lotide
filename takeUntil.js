337cb3ddc59aa1e4ffe3e67098379d6972d95c51
Shadrak Lagadi <shadrl19@gmail.com>
2023-11-29 10:27:04 -0700

    linted

--- takeUntil.js
+++ takeUntil.js
@@ -28,7 +28,7 @@ const takeUntil = function(array, callback) {
       results.push(item);
     }
   }
-}
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -40,4 +40,4 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-assertArrayEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ])
+assertArrayEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
