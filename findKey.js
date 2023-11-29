e9540262ffc7cc576508d7c30a68ca49cab3e4d7
Shadrak Lagadi <shadrl19@gmail.com>
2023-11-29 11:50:46 -0700

    linted and added return undefined if no matches are found

--- findKey.js
+++ findKey.js
@@ -8,12 +8,13 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(object, callback) {
   for (const key of Object.keys(object)) {
-    const objValue = object[key]
+    const objValue = object[key];
     if (callback(objValue)) {
       return key;
     }
   }
-}
+  return undefined;
+};
 
 const result = findKey({
   "Blue Hill": { stars: 1 },
@@ -22,6 +23,6 @@ const result = findKey({
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)
+}, x => x.stars === 2);
 
 assertEqual(result, "noma");
