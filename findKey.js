9cf45a2d09b0bfad4dab8150587637bb3ce63c1c
Shadrak Lagadi <shadrl19@gmail.com>
2023-11-29 11:47:47 -0700

    complete

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😢Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key of Object.keys(object)) {
+    const objValue = object[key]
+    if (callback(objValue)) {
+      return key;
+    }
+  }
+}
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(result, "noma");
