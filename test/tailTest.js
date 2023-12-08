11e02a5e2752c280226b9c51ffbcd6e7237ab85b
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-08 11:15:32 -0700

    added more test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -4,3 +4,7 @@ const assertEqual = require('../assertEqual')
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words)[0], "Lighthouse") // first element of returned array should be second element of argument array
+assertEqual(tail(words).length, words.length - 1) // tail should have one element less than its input
+assertEqual(tail(["test"]).length, 0) // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0) // tail of an empty array should also be empty
