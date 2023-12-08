f10d377389c79c8eff25cc22fdf00f46efc500f9
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-08 14:14:55 -0700

    refactor with exports and require

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const assertArraysEqual = function(given, needed) {
-  const arraysEqual = eqArrays(given, needed);
-  if (arraysEqual) {
-    console.log(`😊 Assertion Passed: [${given}] === [${needed}]`);
-  } else {
-    console.log(`😢 Assertion Failed: [${given}] !== [${needed}]`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
 const middle = function(arr) {
   let noMid = []
   let midElement = [];
@@ -36,9 +15,5 @@ const middle = function(arr) {
   }
   return midElement;
 };
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
- 
+
+module.exports = middle;
