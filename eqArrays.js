331d085be1ae60f4c6e31ebac438c62f2690582a
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-08 12:34:42 -0700

    created eqArraysTest practicing export and require

--- eqArrays.js
+++ eqArrays.js
@@ -9,18 +9,4 @@ const eqArrays = function(arr1, arr2){
   }
   return true;
 }
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😊Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😢Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+module.exports = eqArrays;
--- head.js
+++ head.js
@@ -1,6 +1,7 @@
 // createa function called "head"
 // the head function should NOT return the first element as an array. It should return just the element.
 // an emtpy array should return undefined
+module.exports = head;
 
 function head(actual) {
   if (actual.length === 0) {
@@ -8,5 +9,3 @@ function head(actual) {
   }   
  return actual[0];
 }
-
-module.exports = head;
