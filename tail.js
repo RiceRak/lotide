039aa4ca23bd2120239662b69e9b5c0fe7b8aeac
Shadrak Lagadi <shadrl19@gmail.com>
2023-12-08 11:13:07 -0700

    added tailTest and exported needed modules

--- tail.js
+++ tail.js
@@ -1,23 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😊Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😢Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 function tail(actual) {
-  let myarr = []
-  for (let index = 1; index < actual.length; index++) {
-    myarr.push(actual[index]);
-  }
-  return myarr
+  myArr = actual.slice(1);
+  return myArr;
 }
 
+module.exports = tail;
 
-
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
+// Test Case: Check the original array -- moved to /test/tailTest.js
